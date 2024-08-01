9f74161491c542bdb4c3951621b581afbe6141d1
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-08-01 11:14:46 -0400

    remove nowrap

--- app/ui/travel/phrases.tsx
+++ app/ui/travel/phrases.tsx
@@ -7,12 +7,12 @@ export const Phrases = ({ phrases }: { phrases: Phrase[] }) => {
       <tbody className="bg-white divide-y ">
         {phrases.map((item, index) => (
           <tr key={index}>
-            <td className="px-6 py-4 whitespace-nowrap">
+            <td className="px-6 py-4 ">
               <div className="flex items-center">
                 <div className="text-sm font-medium ">{item.english}</div>
               </div>
             </td>
-            <td className="px-6 py-4 whitespace-nowrap">
+            <td className="px-6 py-4 ">
               <div className="text-sm ">{item.local}</div>
               <div className="text-sm ">{item.englishPronunciation}</div>
             </td>
