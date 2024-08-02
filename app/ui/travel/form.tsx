6c3bec30b81bc7b03c19d3d969eea5a8ad19600b
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-08-02 09:45:14 -0400

    remove unused div

--- app/ui/travel/form.tsx
+++ app/ui/travel/form.tsx
@@ -249,7 +249,6 @@ export const Form = ({ rates }: Props) => {
           </div>
         </div>
         {rate && <EmergencyNumbers rate={rate} />}
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12"></div>
       </div>
     </main>
   );
