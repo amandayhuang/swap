999ad327a76bf3fae9b2e93f8a2bad2769e52eb4
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-08-06 11:30:54 -0400

    test if works probably not

---
+++ app/~offline/page.tsx
@@ -0,0 +1,9 @@
+// pages/_offline.tsx
+export default function Offline() {
+  return (
+    <div>
+      <h1>You are offline</h1>
+      <p>Please check your internet connection.</p>
+    </div>
+  );
+}
