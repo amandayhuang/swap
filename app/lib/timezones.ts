414c0e1835a5688e67daaa80cebd7e976a96b30b
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-08-11 10:06:17 -0400

    add tz constant

---
+++ app/lib/timezones.ts
@@ -0,0 +1,11 @@
+export const timezones = [
+  { name: "NYC", value: "America/New_York" },
+  { name: "LA", value: "America/Los_Angeles" },
+  { name: "London", value: "Europe/London" },
+  { name: "Tokyo", value: "Asia/Tokyo" },
+  { name: "Shanghai", value: "Asia/Shanghai" },
+  { name: "Sydney", value: "Australia/Sydney" },
+  { name: "Mexico City", value: "America/Mexico_City" },
+  { name: "Amsterdam", value: "Europe/Amsterdam" },
+  { name: "Cairo", value: "Africa/Cairo" },
+];
