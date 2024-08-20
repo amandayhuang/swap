c883b4c0eff1bec09aa396d94016aabbb22719b3
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-08-20 10:59:11 -0700

    remove pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import withPWA from "next-pwa";
-const nextConfig = withPWA({
-  dest: "public",
-});
+const nextConfig = {};
 
 export default nextConfig;
